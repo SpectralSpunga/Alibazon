d5cc17595713524d35cedc50fdf6d93f229b31b8
Vyacheslav <75divoda@gmail.com>
2020-10-06 22:13:11 +0300

    Delete productsCatalog.js

--- routes/productsCatalog.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const productsCatalogController = require('../controllers/productsCatalogController');
-
-router.get('/products/:subsubCategory', productsCatalogController.products);
-
-module.exports = router;
